4aed12b7a93b7a4927ee45d8bcc16d59218a8287
Muneeb1294 <56221073+Muneeb1294@users.noreply.github.com>
2025-02-05 12:37:33 +0500

    updates

---
+++ lib/constants/string_constants.dart
@@ -0,0 +1,6 @@
+class StringConstants {
+  static const String placeholderUrl =
+      "https://jsonplaceholder.typicode.com/posts";
+  static const String openAIURL = "https://api.openai.com/v1/chat/completions";
+  static const String apiKey = "";
+}
